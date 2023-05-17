16c36716f4810c127efd1ed1c4d13f2db010a97c
Martin <harleym4rt1n@gmail.com>
2023-05-17 14:20:17 +0800

    modified the encoder

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
-// import "example/analyzer/signature"
+import "example/analyzer/signature"
 
 func main() {
-	detect()
+	// detect()
 	// accountTest()
 	// signature.Main()
+	signature.Main()
 }
--- signature/encode.go
+++ signature/encode.go
@@ -2,8 +2,11 @@ package signature
 
 import (
 	"bufio"
+	"bytes"
 	"container/list"
+	"encoding/gob"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -11,29 +14,47 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func Main() {
-	file, err := os.Open("signature/methods.txt")
+type Combine struct {
+	Sigs   string
+	Method string
+}
+
+func readMethod(address string) []Combine {
+	file, err := os.Open(address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	lis := list.New()
+
+	// lis := list.New()
+	lis := []Combine{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		transferSignature := []byte(line)
 		hash := sha3.NewLegacyKeccak256()
 		hash.Write(transferSignature)
 		methodId := hash.Sum(nil)[:4]
-		lis.PushBack(hexutil.Encode(methodId))
-		fmt.Println(hexutil.Encode(methodId))
+
+		var combine Combine
+		combine.Method = line
+		combine.Sigs = hexutil.Encode(methodId)
+
+		// lis.PushBack(combine)
+		lis = append(lis, combine)
+		// fmt.Println(hexutil.Encode(methodId))
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	return lis
+}
+
+// Element de-duplication
+func deDuplicate(lis *list.List) *list.List {
 	seen := make(map[interface{}]bool)
 	newL := list.New()
 	for e := lis.Front(); e != nil; e = e.Next() {
@@ -43,13 +64,65 @@ func Main() {
 		}
 	}
 
-	outFile, err := os.Create("signature/sigs.txt")
+	return newL
+}
+
+func printSig(address string, lis []Combine) {
+	// gob.RegisterName("Combine", Combine{}) //注册gob
+
+	outFile, err := os.Create(address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer outFile.Close()
 
-	for item := newL.Front(); item != nil; item = item.Next() {
-		fmt.Fprintln(outFile, item.Value)
+	// var buf bytes.Buffer
+	encoder := gob.NewEncoder(outFile)
+
+	if err := encoder.Encode(lis); err != nil {
+		log.Fatal(err)
+		return
 	}
+
+	// if err := ioutil.WriteFile(address, buf.Bytes(), 0644); err != nil {
+	// 	log.Fatal(err)
+	// 	return
+	// }
+
+	// for item := lis.Front(); item != nil; item = item.Next() {
+	// 	fmt.Fprintln(outFile, item.Value)
+	// }
+}
+
+func readStruct(address string) {
+	file, err := ioutil.ReadFile(address)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var result *list.List
+	decoder := gob.NewDecoder(bytes.NewReader(file))
+	if err := decoder.Decode(&result); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(result)
+	// scanner := bufio.NewScanner(file)
+
+	// for scanner.Scan() {
+	// 	var combine Combine
+	// 	combine = scanner.Text()
+	// }
+}
+
+func Main() {
+	lis := readMethod("signature/methods.txt")
+
+	// newL := deDuplicate(lis)
+
+	printSig("signature/struct.gob", lis)
+
+	// readStruct("signature/struct.txt")
 }
